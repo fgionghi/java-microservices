4bad1877e06a6db3d3649fe9a041ce4c10fbadc4
Jonghi <francesco.gionghi@gmail.com>
2019-07-15 09:41:02 +0200

    load balancer

--- Acquisti/src/main/java/com/example/demo/Service/ProductService.java
+++ Acquisti/src/main/java/com/example/demo/Service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.demo.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
@@ -9,23 +10,20 @@ import com.example.demo.domain.Product;
 
 @Service
 public class ProductService {
-	@Value("${product.endpoint}")
-	private String endpoint;
 	
-	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired
+	private RestTemplate restTemplate;
 	
-	public Product getProduct (String productId) {
-		return restTemplate.getForObject(endpoint + "/api/product/{productId}", Product.class, productId);
+	public Product getProducts(String productId) {
+		return restTemplate.getForObject("http://catalogo/api/product/{productId}", Product.class, productId);
 	}
 	
 	public Product bookAvailability (String productId, int quantity) {
-		return restTemplate.exchange(
-				endpoint + "/api/product/{productId}/availability/{quantity}",
+		return restTemplate.exchange("http://catalogo/api/product/{productId}/availability/{quantity}",
 				HttpMethod.PUT,
 				null, 
 				Product.class, 
 				productId, 
 				-quantity).getBody();
-				
 	}
 }
